1d52a1211352f17d8db448e6a8a7d1cdbeed2f79
bpetroski <brenden.petroski@gmail.com>
2024-02-27 21:38:05 -0500

    init

---
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/maps/MapsViewModel.kt
@@ -0,0 +1,12 @@
+package com.travelbetadisaster.travel_log.ui.maps
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MapsViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is map Fragment"
+    }
+    val text: LiveData<String> = _text
+}
