49f3c4fbe5568612791fc3d7e47e50e6a5bc33b2
keeqan <143956080+keeqan@users.noreply.github.com>
2024-03-20 19:15:57 -0400

    Update HomeFragment.kt
    HomeFragment.kt

--- app/src/main/java/com/travelbetadisaster/travel_log/ui/home/HomeFragment.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/home/HomeFragment.kt
@@ -4,52 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.travelbetadisaster.travel_log.R
 import com.travelbetadisaster.travel_log.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val buttonToJournalList = view.findViewById<Button>(R.id.button_to_journal_list)
-        val buttonToProfile = view.findViewById<Button>(R.id.button)
-        val buttonToMaps = view.findViewById<Button>(R.id.navigateToMaps)
-
-        buttonToJournalList.setOnClickListener {
-            findNavController().navigate(R.id.action_nav_home_to_journalListFragment)
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            binding.textHome.text = it
         }
 
-        buttonToProfile.setOnClickListener {
-            findNavController().navigate(R.id.action_nav_home_to_profileFragment)
+        setupListeners()
+
+        return root
+    }
+    // Set Up Listeners for on click buttons to navigate
+    private fun setupListeners() {
+        binding.journalEntryButton.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_journalEntryFragment)
         }
 
-        buttonToMaps.setOnClickListener {
+        binding.mapsButton.setOnClickListener {
             findNavController().navigate(R.id.action_nav_home_to_nav_maps)
         }
 
-        return view
+        // Add more listeners
     }
 
     override fun onDestroyView() {
