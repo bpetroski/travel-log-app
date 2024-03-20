20167f66c4d3438399b23176f3dc0e37820ac6ae
keeqan <143956080+keeqan@users.noreply.github.com>
2024-03-20 19:15:57 -0400

    Update HomeFragment.kt
    HomeFragment.kt

--- app/src/main/java/com/travelbetadisaster/travel_log/ui/home/HomeFragment.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/home/HomeFragment.kt
@@ -4,35 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.travelbetadisaster.travel_log.R
 import com.travelbetadisaster.travel_log.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            binding.textHome.text = it
         }
+
+        setupListeners()
+
         return root
+    }
+    // Set Up Listeners for on click buttons to navigate
+    private fun setupListeners() {
+        binding.journalEntryButton.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_journalEntryFragment)
+        }
+
+        binding.mapsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_nav_maps)
+        }
+
+        // Add more listeners
     }
 
     override fun onDestroyView() {
