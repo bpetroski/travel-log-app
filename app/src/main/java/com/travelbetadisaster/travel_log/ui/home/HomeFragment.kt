6bd1fc4e29bf1e822d3299e807c7d77fc8d50826
EricWill0829 <ewillegg@aol.com>
2024-03-20 19:28:11 -0400

    updates

--- app/src/main/java/com/travelbetadisaster/travel_log/ui/home/HomeFragment.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/home/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.travelbetadisaster.travel_log.R
 import com.travelbetadisaster.travel_log.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,17 +24,32 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val buttonToJournalList = view.findViewById<Button>(R.id.button_to_journal_list)
+        val buttonToProfile = view.findViewById<Button>(R.id.button)
+        val buttonToMaps = view.findViewById<Button>(R.id.navigateToMaps)
+
+        buttonToJournalList.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_journalListFragment)
         }
-        return root
+
+        buttonToProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_profileFragment)
+        }
+
+        buttonToMaps.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_nav_maps)
+        }
+
+        return view
     }
 
     override fun onDestroyView() {
