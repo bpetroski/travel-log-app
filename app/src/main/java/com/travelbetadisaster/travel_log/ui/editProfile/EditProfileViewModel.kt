f32303261249385d992b6b456be6ad67d918ff70
Pkmartus <pkmartus@gmail.com>
2024-03-24 10:45:07 -0400

    Created new fragments and viewmodels for the edit views.

---
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/editProfile/EditProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.travelbetadisaster.travel_log.ui.editProfile
+
+import androidx.lifecycle.ViewModel
+
+class EditProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
