adb7bdec3ac518f6dfdff1470cfa0a30ab5b2038
EricWill0829 <ewillegg@aol.com>
2024-03-28 21:18:51 -0400

    updates for Journal List

--- app/src/main/java/com/travelbetadisaster/travel_log/ui/journalList/JournalListFragment.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/journalList/JournalListFragment.kt
@@ -7,14 +7,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.travelbetadisaster.travel_log.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.travelbetadisaster.travel_log.databinding.FragmentJournalListBinding
+import com.travelbetadisaster.travel_log.ui.journalList.adapter.JournalListAdapter
+import com.travelbetadisaster.travel_log.ui.journalList.viewModel.JournalListViewModel
+
 
 class JournalListFragment : Fragment() {
+
+    private lateinit var adapter: JournalListAdapter
+    private lateinit var viewModel: JournalListViewModel
+    private var _binding: FragmentJournalListBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = JournalListFragment()
     }
-
-    private lateinit var viewModel: JournalListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +32,49 @@ class JournalListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_journal_list, container, false)
     }
 
+    listenerSetup()
+    observerSetup()
+    recyclerSetup()
+
+    return view
+}
+
+private fun listenerSetup() {
+    binding.cardView.setOnClickListener { onCardViewClick() }
+}
+
+private fun observerSetup() {
+    viewModel.journalList.observe(viewLifecycleOwner) { journals ->
+        adapter.submitList(journals)
+    }
+}
+
+private fun recyclerSetup() {
+    adapter = JournalListAdapter()
+    binding.recyclerView.adapter = adapter
+    binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+    adapter.setOnItemClickListener { journal ->
+        onListClick(journal)
+    }
+}
+
+private fun onListClick(journal: Journal) {
+}
+
+private fun onCardViewClick() {
+}
+
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(JournalListViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
     }
 
 }
