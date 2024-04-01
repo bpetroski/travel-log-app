0baa8134a1227f0f20dcb1f92acdf6991369fe65
Pkmartus <115840180+Pkmartus@users.noreply.github.com>
2024-04-01 10:34:15 -0400

    Update Visit.kt

--- app/src/main/java/com/travelbetadisaster/travel_log/database/tables/Visit.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/database/tables/Visit.kt
@@ -1,4 +1,58 @@
 package com.travelbetadisaster.travel_log.database.tables
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "visits")
+
 class Visit {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "VisitID")
+    var id:Int = 0
+
+    @ColumnInfo(name = "name")
+    var name:String? = null
+
+    @ColumnInfo(name = "location_id")
+    var location:Int = 0
+
+    @ColumnInfo(name = "image")
+    var image:String? = null
+
+    @ColumnInfo(name = "text")
+    var text:String? = null
+
+    @ColumnInfo(name = "trip_id")
+    var tripID:Int = 0
+
+    constructor() {}
+
+    constructor(id: Int, name: String, location: Int,
+                image: String, text: String,
+                tripID: Int){
+
+        this.id = id
+        this.name = name
+        this.location = location
+        this.image = image
+        this.text = text
+        this.tripID = tripID
+
+    }
+
+    constructor(name: String, location: Int,
+                image: String, text: String,
+                tripID: Int){
+
+        this.name = name
+        this.location = location
+        this.image = image
+        this.text = text
+        this.tripID = tripID
+
+    }
+
+
 }
