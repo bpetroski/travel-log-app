c38cc8fcbc1c1d67e3beb6d20279d87da78ca160
Pkmartus <115840180+Pkmartus@users.noreply.github.com>
2024-04-01 10:31:53 -0400

    Update User.kt

--- app/src/main/java/com/travelbetadisaster/travel_log/database/tables/User.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/database/tables/User.kt
@@ -13,10 +13,20 @@ class User {
 
     @ColumnInfo(name = "name")
     var name:String? = null
+
+    @ColumnInfo(name = "profilePic")
     var profilePic:String? = null
+
+    @ColumnInfo(name = "bio")
     var bio:String? = null
+
+    @ColumnInfo(name = "homeLocation")
     var homeLocation:String? = null
+
+    @ColumnInfo(name = "email")
     var email:String? = null
+
+    @ColumnInfo(name = "password")
     var password:String? = null
 
     constructor() {}
