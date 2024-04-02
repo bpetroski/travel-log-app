4c8a2866914fdd13ce786ad59186c74b33437e98
SarahLappin0420 <sarahjeanlappin0420@gmail.com>
2024-04-02 12:14:15 -0400

    sl

--- app/src/main/java/com/travelbetadisaster/travel_log/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/profile/ProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.travelbetadisaster.travel_log.R
 
+
 class ProfileFragment : Fragment() {
 
     companion object {
@@ -16,17 +17,54 @@ class ProfileFragment : Fragment() {
 
     private lateinit var viewModel: ProfileViewModel
 
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        viewModel = ViewModelProvider(this).get(ProfileViewModelViewModel::class.java)
+
+
+        listenerSetup()
+
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun listenerSetup() {
+        binding.editButton.setOnClickListener { onEditClick() }
+        binding.saveButton.setOnClickListener { onSaveClick() }
+        binding.xButton.setOnClickListener { onXClick() }
+        binding.historyButton.setOnClickListener { onHistoryClick() }
+    }
+    private fun onEditClick() {
+        // Handle edit button click
+
     }
 
+    private fun onSaveClick() {
+        // Handle save button click
+
+    }
+
+    private fun onXClick() {
+        // Handle X button click
+
+    }
+
+    private fun onHistoryClick() {
+        // Handle history button click
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
