47d8d4336ab4ffacc7d12f11e0f64112604f9844
EricWill0829 <ewillegg@aol.com>
2024-04-03 18:07:06 -0400

    updates to Edit Profile

--- app/src/main/java/com/travelbetadisaster/travel_log/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/profile/EditProfileFragment.kt
@@ -6,21 +6,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.travelbetadisaster.travel_log.R
+import com.travelbetadisaster.travel_log.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : Fragment() {
+
+    private lateinit var viewModel: ProfileViewModel
+    private var _binding: FragmentEditProfileBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = EditProfileFragment()
     }
-
-    private lateinit var viewModel: ProfileViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        listenerSetup()
+    }
+
+    private fun listenerSetup() {
+        binding.saveButton.setOnClickListener { onSaveClick() }
+        binding.cancelButton.setOnClickListener { onXClick() }
+    }
+
+    private fun onSaveClick() {
+        viewModel.setNewName(binding.editTextName.text.toString())
+        viewModel.setNewHomeTown(binding.editTextHomeTown.text.toString().toInt())
+        viewModel.setNewDescription(binding.editTextDescription.text.toString())
+        viewModel.updateUserProfile()
+        Toast.makeText(requireContext(), "Profile updated!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onXClick() {
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/travelbetadisaster/travel_log/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/profile/ProfileFragment.kt
@@ -1,12 +1,16 @@
 package com.travelbetadisaster.travel_log.ui.profile
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.travelbetadisaster.travel_log.R
+import com.travelbetadisaster.travel_log.database.tables.User
+
 
 class ProfileFragment : Fragment() {
 
@@ -15,18 +19,36 @@ class ProfileFragment : Fragment() {
     }
 
     private lateinit var viewModel: ProfileViewModel
+    private lateinit var textViewName: TextView
+    private lateinit var textViewHomeTown: TextView
+    private lateinit var textViewDescription: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        textViewName = view.findViewById(R.id.textViewName)
+        textViewHomeTown = view.findViewById(R.id.textViewHomeTown)
+        textViewDescription = view.findViewById(R.id.textViewDescription)
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.user.observe(viewLifecycleOwner, Observer { user ->
+            user?.let { updateUserProfileUI(it) }
+        })
+    }
+
+    private fun updateUserProfileUI(user: User) {
+        textViewName.text = user.name
+        textViewHomeTown.text = user.hometown
+        textViewDescription.text = user.description
     }
 
 }
