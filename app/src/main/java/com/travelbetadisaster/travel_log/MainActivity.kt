fbe4226ee1d10dade785e5b3689369832ce78535
bpetroski <brenden.petroski@gmail.com>
2024-04-03 02:10:10 -0400

    map view model code
    Signed-off-by: bpetroski <brenden.petroski@gmail.com>

--- app/src/main/java/com/travelbetadisaster/travel_log/MainActivity.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
+        binding.appBarMain.newJournalEntry.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_maps
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
