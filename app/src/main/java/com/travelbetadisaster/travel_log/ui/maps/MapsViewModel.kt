da18c97e04b655e89a7972c7850eb1d72660b57a
keeqan <143956080+keeqan@users.noreply.github.com>
2024-04-03 12:05:28 -0400

    Update MapsViewModel.kt

--- app/src/main/java/com/travelbetadisaster/travel_log/ui/maps/MapsViewModel.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/ui/maps/MapsViewModel.kt
@@ -1,12 +1,32 @@
-package com.travelbetadisaster.travel_log.ui.maps
-
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.travelbetadisaster.travel_log.database.dao.SharedPreferencesMapDao
+import com.travelbetadisaster.travel_log.database.repositories.JournalRepository
+import com.travelbetadisaster.travel_log.database.repositories.MapRepository
+import com.travelbetadisaster.travel_log.database.tables.Visit
 
-class MapsViewModel : ViewModel() {
+class MapsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val journalRepository: JournalRepository = JournalRepository(application)
+    val allEntries: LiveData<List<Visit>>? = journalRepository.allVisits
+
+    private val mapRepository: MapRepository
+
+    init {
+        val sharedPreferences = application.getSharedPreferences("app_preferences", Application.MODE_PRIVATE)
+        val mapDao = SharedPreferencesMapDao(sharedPreferences)
+        mapRepository = MapRepository(mapDao)
+    }
+
     private val _text = MutableLiveData<String>().apply {
         value = "This is map Fragment."
     }
     val text: LiveData<String> = _text
+
+    fun getLastKnownLocation(): LatLng? = mapRepository.getLastKnownUserLocation()
+
+    fun saveLocation(location: LatLng) = mapRepository.saveUserLocation(location)
 }
