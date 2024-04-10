685e89126bc684faba359f13ef67a30167ec846c
bpetroski <brenden.petroski@gmail.com>
2024-04-10 18:21:52 -0400

    bpetroski - Testing:
    Signed-off-by: bpetroski <brenden.petroski@gmail.com>

--- app/src/main/java/com/travelbetadisaster/travel_log/MainActivity.kt
+++ app/src/main/java/com/travelbetadisaster/travel_log/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-               R.id.nav_home, R.id.nav_journal_list, R.id.nav_bucket_list, R.id.nav_maps
+               R.id.nav_home, R.id.nav_profile, R.id.nav_journal_list, R.id.nav_bucket_list, R.id.nav_maps
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
