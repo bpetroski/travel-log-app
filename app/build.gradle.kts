89bc4571a64bcecada3cc6d9375a6339419e280a
keeqan <keegan.miles20@gmail.com>
2024-04-17 17:17:06 -0400

    EditProfileFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,14 +64,15 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation ("com.google.code.gson:gson:2.8.6")
+    implementation ("com.google.android.libraries.places:places:3.4.0")
 
 
 
-    implementation ("androidx.room:room-runtime:2.4.3")
-    implementation ("androidx.room:room-ktx:2.4.3")
-    kapt ("androidx.room:room-compiler:2.4.3")
-    implementation ("androidx.activity:activity-ktx:1.6.0")
-    annotationProcessor ("android.arch.persistence.room:compiler:1.1.1")
+
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.activity:activity-ktx:1.8.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
